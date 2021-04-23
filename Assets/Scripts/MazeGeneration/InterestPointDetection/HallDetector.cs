d171eb29f9a2bbade1f76c3350897bb40d815377
miguel <miguelcok27@gmail.com>
2021-04-22 22:37:49 -0400

    fix: HallDetector

--- Assets/Scripts/MazeGeneration/InterestPointDetection/HallDetector.cs
+++ Assets/Scripts/MazeGeneration/InterestPointDetection/HallDetector.cs
@@ -12,27 +12,39 @@ namespace MazeGeneration.InterestPointDetection
             var floorPositions = maze.GetFloorPositions();
             var tilePos = new Vector3Int();
             var points = new List<Vector3Int>();
-            
+
+            int[] tilesValues = new int[9];
             foreach (var tilePosition in floorPositions)
             {
                 int neighbourValue = 0;
+                int tileIndex = 0;
                 for (int i = -1; i <= 1; i++)
                 {
+                    int absI = Math.Abs(i);
+                    
                     for (int j = -1; j <= 1; j++)
                     {
-                        if (i != 0 || j != 0)
-                        {
-                            tilePos.x = tilePosition.x + i;
-                            tilePos.y = tilePosition.y + j;
-                            if (maze.FloorSpriteTilemap.HasTile(tilePos))
-                            {
-                                neighbourValue += Math.Abs(i);
-                                neighbourValue += Math.Abs(j);
-                            }
-                        }
+                        int absJ = Math.Abs(j);
+                        
+                        tilePos.x = tilePosition.x + i;
+                        tilePos.y = tilePosition.y + j;
+                        int hasTile = maze.FloorSpriteTilemap.HasTile(tilePos) ? 1 : 0;
+                        int xor = absI ^ absJ;  // 1 if is left, right, top or bottom neighbour, 0 otherwise
+                        neighbourValue += xor * hasTile;
+
+                        tilesValues[tileIndex] = hasTile;
+
+                        tileIndex++;
                     }
                 }
-                if (neighbourValue > 0 && neighbourValue <= 2)
+
+                bool cornerSquare = tilesValues[0] + tilesValues[1] + tilesValues[3] == 3;
+                cornerSquare |= tilesValues[2] + tilesValues[1] + tilesValues[5] == 3;
+                cornerSquare |= tilesValues[6] + tilesValues[3] + tilesValues[7] == 3;
+                cornerSquare |= tilesValues[8] + tilesValues[5] + tilesValues[7] == 3;
+
+                bool isHall = neighbourValue > 0 && neighbourValue <= 2;
+                if (isHall && !cornerSquare)
                 {
                     points.Add(tilePosition);
                 }
