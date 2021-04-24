ce18bdd01bce0e969f859eb69a6276afc33be3b9
miguel <miguelcok27@gmail.com>
2021-04-24 09:22:58 -0400

    implemented: camera follows character

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class CameraFollow : MonoBehaviour
+    {
+        [SerializeField] private Transform target;
+
+        [Range(0.0f, 1.0f)]
+        [SerializeField] private float speed;
+
+        private void Update()
+        {
+            MoveTowards();
+        }
+
+        private void MoveTowards()
+        {
+            var targetPosition = target.position;
+            var selfPosition = transform.position;
+
+            Vector3 newPosition = Vector2.Lerp(selfPosition, targetPosition, speed);
+            newPosition.z = selfPosition.z;
+            transform.position = newPosition;
+        }
+    }
+}
