80cd8f966e53663ff40e6109d08ab57c917870a8
miguel <miguelcok27@gmail.com>
2021-04-25 19:54:58 -0400

    feature: walls go down when near player fix: near elements pop-out code optimized

---
+++ Assets/Scripts/UpdateShadersPlayerPosition.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdateShadersPlayerPosition : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    [SerializeField] private List<Material> materials;
+
+    private void Update()
+    {
+        foreach (var material in materials)
+        {
+            material.SetVector("_PlayerPos", target.transform.position);
+        }
+    }
+}
