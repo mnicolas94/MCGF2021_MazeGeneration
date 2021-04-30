1128149748f5486e81a18d2a179731c798a71239
miguel <miguelcok27@gmail.com>
2021-04-30 16:51:44 -0400

    implemeted: LevelTransitionFirstPhase coroutine

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using Character;
 using MazeGeneration;
 using UnityEngine;
 
@@ -9,6 +11,16 @@ public class GameManager : MonoBehaviour
     public static GameManager Instance => _instance;
 
     [SerializeField] private MazeController mazecontroller;
+    
+    [SerializeField] private PlayerController playerController;
+    [SerializeField] private SpriteRenderer playerRenderer;
+
+    [SerializeField] private Canvas blackBackgroundCanvas;
+    [SerializeField] private int blackBackgroundSortingOrder;
+    
+    [SerializeField] private CharacterRuntimeData lineOfSightData;
+    [Range(0.0f, 1.0f)] [SerializeField] private float lineOfSightLerpSpeed;
+    
     
     private void Awake()
     {
@@ -22,8 +34,69 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    private void Start()
+    {
+        blackBackgroundCanvas.gameObject.SetActive(false);
+    }
+
+    [NaughtyAttributes.Button]
     public void NotifyPuzzleSolved()
     {
+        StartCoroutine(LevelTransitionFirstPhase());
+    }
+
+    private IEnumerator LevelTransitionFirstPhase()
+    {
+        // desabilitar input
+        playerController.enabled = false;
+        
+        // wait some time e.g. 1 sec
+        yield return new WaitForSeconds(1);
+        
+        // animación reducir LoS
+        float currentLineOfSightRadius = lineOfSightData.LineOfSightRadius;
+        yield return LerpLineOfSightToTargetValue(0);
+        
+        // selección de items
+        
+        // seleccionar puzzles y ponerselos al maze controller
+        
+        // hide maze
+        int playerSortingOrder = playerRenderer.sortingOrder;
+        playerRenderer.sortingOrder = blackBackgroundSortingOrder;
+        blackBackgroundCanvas.gameObject.SetActive(true);
+
+        // generar maze
         mazecontroller.GenerateMaze();
+
+        yield return null;
+        
+        // show maze
+        playerRenderer.sortingOrder = playerSortingOrder;
+        blackBackgroundCanvas.gameObject.SetActive(false);
+        
+        // posicionar personaje en suelo (y camara en personaje)
+        
+        // retroalimentación de progreso
+        
+        // animación aumentar LoS
+        yield return LerpLineOfSightToTargetValue(currentLineOfSightRadius);
+
+        // habilitar input
+        playerController.enabled = true;
+    }
+
+    private IEnumerator LerpLineOfSightToTargetValue(float losTarget)
+    {
+        float dif = Mathf.Abs(lineOfSightData.LineOfSightRadius - losTarget);
+        while (dif > 1E-3)
+        {
+            float lerpValue = Mathf.Lerp(lineOfSightData.LineOfSightRadius, losTarget, lineOfSightLerpSpeed);
+            lineOfSightData.SetLineOfSightRadius(lerpValue);
+            dif = Mathf.Abs(lineOfSightData.LineOfSightRadius - losTarget);
+            yield return null;
+        }
+        
+        lineOfSightData.SetLineOfSightRadius(losTarget);
     }
 }
