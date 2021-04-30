968e9dcffc88915c76b60f7f20fb820bce6c0ccb
miguel <miguelcok27@gmail.com>
2021-04-30 15:26:49 -0400

    implemented: puzzle levers created: GameManager class

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using MazeGeneration;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager _instance;
+
+    public static GameManager Instance => _instance;
+
+    [SerializeField] private MazeController mazecontroller;
+    
+    private void Awake()
+    {
+
+        if (_instance == null)
+        {
+            _instance = this;
+        } else if (_instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void NotifyPuzzleSolved()
+    {
+        mazecontroller.GenerateMaze();
+    }
+}
