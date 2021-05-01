bdcb00c30f4985735dbc7b9d4d6cb7f17d238a99
miguel <miguelcok27@gmail.com>
2021-05-01 10:13:32 -0400

    make pop up on player near variables global

--- Assets/Scripts/Character/LineOfSightData.cs
+++ Assets/Scripts/Character/LineOfSightData.cs
@@ -3,14 +3,18 @@
 namespace Character
 {
     [CreateAssetMenu(fileName = "CharacterRuntimeData", menuName = "Character/CharacterRuntimeData", order = 0)]
-    public class CharacterRuntimeData : ScriptableObject
+    public class LineOfSightData : ScriptableObject
     {
         private Vector3 _characterPosition;
         
         [SerializeField] private float lineOfSightRadius;
         [SerializeField] private float horizontalScale;
         [SerializeField] private Vector3 lineOfSightOffset;
-
+        
+        [SerializeField] private float animDuration;
+        [SerializeField] private AnimationCurve popOutCurve;
+        [SerializeField] private AnimationCurve popInCurve;
+        
         public Vector3 CharacterPosition => _characterPosition;
 
         public float LineOfSightRadius => lineOfSightRadius;
@@ -18,6 +22,12 @@ namespace Character
         public Vector3 LineOfSightOffset => lineOfSightOffset;
 
         public float HorizontalScale => horizontalScale;
+
+        public float AnimDuration => animDuration;
+
+        public AnimationCurve PopOutCurve => popOutCurve;
+
+        public AnimationCurve PopInCurve => popInCurve;
 
         public void SetCharacterPosition(Vector3 position)
         {
