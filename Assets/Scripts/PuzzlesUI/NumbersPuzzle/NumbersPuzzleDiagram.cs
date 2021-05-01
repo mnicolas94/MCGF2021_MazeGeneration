b833041c964e72872685c887a4ff2c83aacb736a
miguel <miguelcok27@gmail.com>
2021-05-01 11:09:05 -0400

    implemented: numbers sequence puzzle

---
+++ Assets/Scripts/PuzzlesUI/NumbersPuzzle/NumbersPuzzleDiagram.cs
@@ -0,0 +1,30 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace PuzzlesUI.NumbersPuzzle
+{
+    public class NumbersPuzzleDiagram : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI text;
+
+        private string _generatedSequence;
+
+        public string GeneratedSequence => _generatedSequence;
+
+        private void Start()
+        {
+            _generatedSequence = GenerateSequence();
+            text.text = _generatedSequence;
+        }
+
+        private string GenerateSequence()
+        {
+            int n1 = Random.Range(0, 10);
+            int n2 = Random.Range(0, 10);
+            int n3 = Random.Range(0, 10);
+            int n4 = Random.Range(0, 10);
+
+            return $"{n1}{n2}{n3}{n4}";
+        }
+    }
+}
