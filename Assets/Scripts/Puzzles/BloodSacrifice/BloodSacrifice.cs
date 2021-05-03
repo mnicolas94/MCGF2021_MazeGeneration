108b7ae709666f523b5ff671ff9096089dde4603
miguel <miguelcok27@gmail.com>
2021-05-03 17:10:39 -0400

    implemented: blood sacrifice puzzle

---
+++ Assets/Scripts/Puzzles/BloodSacrifice/BloodSacrifice.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Character;
+using UnityEngine;
+
+namespace Puzzles.BloodSacrifice
+{
+    public class BloodSacrifice : MonoBehaviour
+    {
+        [SerializeField] private Interactable interactable;
+        [SerializeField] private int damage;
+        
+        private void Start()
+        {
+            interactable.eventInteracted.AddListener(OnInteraction);
+        }
+
+        private void OnInteraction()
+        {
+            interactable.gameObject.SetActive(false);
+            var health = GameManager.Instance.PlayerController.GetComponent<Health>();
+            health.DoDamage(damage);
+            GameManager.Instance.NotifyPuzzleSolved();
+        }
+    }
+}
