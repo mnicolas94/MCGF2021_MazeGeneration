8267f0011d0cfc863ad4f7fc2819d7fe232de6ef
miguel <miguelcok27@gmail.com>
2021-05-03 10:09:13 -0400

    implemented: useful console commands

---
+++ Assets/Scripts/Editor/ConsoleCommands/MazeGenerationCommands.cs
@@ -0,0 +1,16 @@
+﻿using IngameDebugConsole;
+using MazeGeneration;
+using UnityEngine;
+
+namespace Editor.ConsoleCommands
+{
+    public class MazeGenerationCommands
+    {
+        [ConsoleMethod("maze.gen_maze", "Generates a maze")]
+        public static void GenerateMaze()
+        {
+            var mazeController = Object.FindObjectOfType<MazeController>();
+            mazeController.GenerateMaze();
+        }
+    }
+}
