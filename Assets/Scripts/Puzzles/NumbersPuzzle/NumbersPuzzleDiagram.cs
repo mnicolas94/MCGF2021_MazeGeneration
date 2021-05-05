8a042bce14262ca1342f59975ac8e9f4c044afdc
miguel <miguelcok27@gmail.com>
2021-05-05 10:26:01 -0400

    modified: numbers puzzle to have 7 digits and generate sequence after each interaction

--- Assets/Scripts/Puzzles/NumbersPuzzle/NumbersPuzzleDiagram.cs
+++ Assets/Scripts/Puzzles/NumbersPuzzle/NumbersPuzzleDiagram.cs
@@ -6,12 +6,19 @@ namespace Puzzles.NumbersPuzzle
     public class NumbersPuzzleDiagram : MonoBehaviour
     {
         [SerializeField] private TextMeshProUGUI text;
+        [SerializeField] private Interactable interactable;
 
         private string _generatedSequence;
 
         public string GeneratedSequence => _generatedSequence;
 
         private void Start()
+        {
+            interactable.eventInteracted.AddListener(GenerateCode);
+            GenerateCode();
+        }
+
+        private void GenerateCode()
         {
             _generatedSequence = GenerateSequence();
             text.text = _generatedSequence;
@@ -23,8 +30,11 @@ namespace Puzzles.NumbersPuzzle
             int n2 = Random.Range(0, 10);
             int n3 = Random.Range(0, 10);
             int n4 = Random.Range(0, 10);
+            int n5 = Random.Range(0, 10);
+            int n6 = Random.Range(0, 10);
+            int n7 = Random.Range(0, 10);
 
-            return $"{n1}{n2}{n3}{n4}";
+            return $"{n1}{n2}{n3}{n4}{n5}{n6}{n7}";
         }
     }
 }
