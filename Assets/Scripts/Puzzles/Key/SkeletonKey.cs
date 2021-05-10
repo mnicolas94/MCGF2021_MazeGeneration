74c05d2a7028f8881f88c273e229e03e54394812
miguel <miguelcok27@gmail.com>
2021-05-10 16:59:16 -0400

    implemented: door key puzzle

---
+++ Assets/Scripts/Puzzles/Key/SkeletonKey.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UI;
+using UnityEngine;
+
+namespace Puzzles.Key
+{
+    public class SkeletonKey : MonoBehaviour
+    {
+        public Action eventInteracted;
+        
+        [SerializeField] private Interactable interactable;
+
+        private void Start()
+        {
+            interactable.eventInteracted.AddListener(OnInteraction);
+        }
+
+        private void OnInteraction()
+        {
+            eventInteracted?.Invoke();
+        }
+    }
+}
