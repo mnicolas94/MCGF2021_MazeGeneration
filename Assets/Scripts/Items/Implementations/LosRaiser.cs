e86e66cca87dd5e39199e707da96c93a9e77c2c2
miguel <miguelcok27@gmail.com>
2021-05-15 13:16:09 -0400

    implemented all items added heal potion decorator implemented data reseter

---
+++ Assets/Scripts/Items/Implementations/LosRaiser.cs
@@ -0,0 +1,17 @@
+﻿using Character;
+using UnityEngine;
+
+namespace Items.Implementations
+{
+    [CreateAssetMenu(fileName = "LoSRaiser", menuName = "Items/LoSRaiser", order = 0)]
+    public class LosRaiser : Item
+    {
+        [SerializeField] private LineOfSightData losData;
+        [SerializeField] private float losAmount;
+        
+        public override void ApplyEffectOnPickUp()
+        {
+            losData.SetMaxLineOfSightRadius(losData.MaxLineOfSightRadius + losAmount);
+        }
+    }
+}
---
+++ Assets/Scripts/Items/Implementations/MaxHealthRaiser.cs
@@ -0,0 +1,18 @@
+﻿using Character;
+using Character.Data;
+using UnityEngine;
+
+namespace Items.Implementations
+{
+    [CreateAssetMenu(fileName = "MaxHealthRaiser", menuName = "Items/MaxHealthRaiser", order = 0)]
+    public class MaxHealthRaiser : Item
+    {
+        [SerializeField] private int healthAmount;
+        
+        public override void ApplyEffectOnPickUp()
+        {
+            var playerHealth = GameManager.Instance.PlayerController.GetComponent<Health>();
+            playerHealth.SetMaxHealth(playerHealth.MaxHealth + healthAmount);
+        }
+    }
+}
